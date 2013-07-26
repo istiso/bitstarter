2ad20629fd14e41c89514310f1c661d6ed53fa8e
Mohammed AlAbbadi <istiso@gmail.com>
2013-07-26 14:27:29 +0000

    added the modified files to check urls

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -65,8 +66,23 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+        .option('-u, --url <url>', 'Path to url')
+	.parse(process.argv);
+   if(program.url){
+	rest.get(program.url).on('complete', function(result) {
+            if (result instanceof Error) {
+                console.log('Error'+ result.message);
+            } else {
+                fs.writeFileSync('index.html', result);
+                    }
+        });
+
+        
+        var checkJson = checkHtmlFile('index.html', program.checks);
+    }
+    else if(program.file) {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+    }	
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
